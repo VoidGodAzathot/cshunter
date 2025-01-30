b54eb00d55993d4b7bda4d8743577143ec7cf890
Maxim <maximscherbakov006@gmail.com>
2025-01-30 18:44:20 +0300

    bug fix

--- src/components/sidebar/sidebar-provider.tsx
+++ src/components/sidebar/sidebar-provider.tsx
@@ -5,7 +5,7 @@ export default function SidebarProvider(page: Page) {
     return (
         <Box backgroundColor="#18181B" borderRadius={20} borderWidth="1.5px" width="full" height="calc(100vh - 50px)">
             <Box className="select-none" padding={5} height="full">
-                { page.source() }
+                <page.source />
             </Box>
         </Box>
     )
